9e51f5119fc29a29900452acd7231c7e1ccf599c
Patric Hollenstein <patric.hollenstein@lustenauer.net>
2018-12-16 20:38:23 +0100

    Initial commit

---
+++ BlockBuilder/MainWindow.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System.Windows;
+
+namespace BlockBuilder
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void OnButtonRun_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Button run clicked");
+        }
+    }
+}
